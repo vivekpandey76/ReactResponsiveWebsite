e4d44fe1a09c8d435658e106f74dd10632d78a74
Vivekpandey76 <Vivekpandey22982@gmail.com>
2021-07-15 19:05:48 +0530

    HeroSection

---
+++ src/Components/Homepage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button } from "./Button";
+import "./Homepage.css";
+
+function Homepage() {
+  return (
+    <div className="hero-container">
+      <video src="/video/home.mp4" autoPlay loop muted />
+      <h1>We make your website look great</h1>
+      <p>What are you waiting for?</p>
+      <div className="hero-btns">
+        <Button buttonStyle="btn--outline" buttonSize="btn--large">
+          Get Started
+        </Button>
+        <Button buttonStyle="btn--primary" buttonSize="btn--large">
+          Watch Trailer
+          <i className="far fa-play-circle" />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ src/Components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../App.css";
+import Homepage from "../Homepage";
+
+function Home() {
+  return (
+    <>
+      <Homepage />
+    </>
+  );
+}
+export default Home;
