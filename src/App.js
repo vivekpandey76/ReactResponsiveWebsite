7c139f1b573ea4488e7aafee7b153158b362bd33
Vivekpandey76 <Vivekpandey22982@gmail.com>
2021-07-16 11:45:43 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/pages/Home";
+import Services from "./Components/pages/Services";
+import Products from "./Components/pages/Products";
+import SignUp from "./Components/pages/SignUp";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={Products} />
+          <Route path="/sign-up" component={SignUp} />
         </Switch>
       </Router>
     </>
--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "../../App.css";
+import Cards from "../Cards";
+import Footer from "../footer";
 import Homepage from "../Homepage";
 
 function Home() {
   return (
     <>
       <Homepage />
+      <Cards />
+      <Footer />
     </>
   );
 }
